bdaf93b7aac7d9b09d0208f928890491a2020618
Keri Schreckengost <keris@live.com>
2017-08-27 16:39:23 -0600

    App listening on port

--- server.js
+++ server.js
@@ -5,16 +5,31 @@ var bodyParser = require ("body-parser");
 
 // **************************************
 //Set up the Express App
+var PORT = process.env.PORT || 3000;
 var app = express();
-var PORT = process.env.PORT || 8000;
 
-//Require models for syncing
-var db = require("./models");
+// Static directory
+app.use(express.static("public"));
 
 //Set up Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-// Static directory
-app.use(express.static("public"));
+
+//Override with POST ?_method=DELETE
+app.use(methodOverride("_method"));
+
+//Set Handlebars
+var exphbs = require ("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Inport routes and give server access
+// require("./controllers/ic_controller")(app);
+
+//Listener - start the server
+app.listen(PORT, function(){
+	console.log("App listening on PORT " + PORT);
+});
