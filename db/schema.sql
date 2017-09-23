3404f53d53d7d1c2e2cebceb2fc25e803b3fd680
Keri Schreckengost <keris@live.com>
2017-09-23 13:17:05 -0600

    heroku

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE iceCream_db;
 USE iceCream_db;
 
 -- Create a table with these fields: (1) id: an auto incrementing int that serves as the primary key, (2) icCream_name: a string, (3) devoured: a boolean, (4) date: a TIMESTAMP.
-CREATE TABLE icecream
+CREATE TABLE iceCream
 (
 	id int NOT NULL auto_increment,
     iceCream_name varchar(255) NOT NULL,
